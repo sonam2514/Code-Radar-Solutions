ad244d4e342b775644c5fed14ea3ad553debb54b
sonam2514 <9888808731sr@gmail.com>
2025-04-11 12:08:13 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -20,6 +20,11 @@ int main(){
         }  
        }
     }
-    printf("%d %d",a,b);
+    if(a<b){
+        printf("%d %d",a,b);
+    }
+    else{
+        printf("%d %d",b,a);
+    }
     return 0;
 }
